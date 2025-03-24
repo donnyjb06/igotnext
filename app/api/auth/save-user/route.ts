a74cbd3458d17dba755d593cf1b2e0c99acda6ac
DonovanJabbar <djabbar51@gmail.com>
2025-03-23 22:33:56 -0600

    refactor: delete references to nextauth

---
+++ app/api/auth/save-user/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
+
+export const POST = async (req: NextRequest) => {
+    try {
+      const { clerkId, email, userName, fullName, position } = await req.json()
+      console.log(clerkId, email, userName, fullName, position);
+
+      const authHeader = req.headers.get("Authorization");
+      const serverSecret = process.env.API_SECRET_KEY;
+
+      if (!authHeader || authHeader !== `Bearer ${serverSecret}`) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+      }
+
+      const user = await prisma.user.create( {
+        data: {
+          clerkId,
+          email,
+          username: userName,
+          fullname: fullName,
+          position,
+        }
+      })
+
+      return NextResponse.json(user, { status: 201 })
+  } catch (error) {
+    console.error("Error saving user:", error)
+    return NextResponse.json(
+      { error: "Failed to save user" },
+      { status: 500 }
+    )
+  }
+}
