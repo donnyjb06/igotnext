1ffe4e878f5e9222423f0ffc77f023c33b2857ea
DonovanJabbar <djabbar51@gmail.com>
2025-03-24 22:42:07 -0600

    style: format file

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,32 +1,51 @@
-import React from 'react'
-import { FormItem, FormLabel, FormMessage, FormControl } from '@/components/ui/form';
+import React from 'react';
+import {
+  FormItem,
+  FormLabel,
+  FormControl,
+} from '@/components/ui/form';
 import { Input } from './ui/input';
 import { Control, Controller, FieldValues, Path } from 'react-hook-form';
 
 interface FormFieldProps<T extends FieldValues> {
-  control: Control<T>,
-  name: Path<T>,
+  control: Control<T>;
+  name: Path<T>;
   label: string;
-  placeholder?: string
-  type?: "text" | "email" | "password"| "file"
+  placeholder?: string;
+  type?: 'text' | 'email' | 'password' | 'file';
 }
 
-const FormField = ( { control, name, label, placeholder, type="text" }: FormFieldProps<T> ) => {
-  return(
-    <Controller name={name} control={control} render={({ field, fieldState }) => (
-      <FormItem className='mb-0'>
-      <FormLabel className='label'>{label}</FormLabel>
-      <FormControl>
-        <Input className="custom-input" 
-          placeholder={placeholder} 
-          {...field} 
-          type={type}/>
-      </FormControl>
-      {fieldState.error ? (
-        <p className='body-text text-red-500 max-w-[250px]'>{fieldState.error.message}</p>
-    </FormItem>
-    )}/>
-  )
-}
+const FormField = <T extends FieldValues>({
+  control,
+  name,
+  label,
+  placeholder,
+  type = 'text',
+}: FormFieldProps<T>) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState }) => (
+        <FormItem className='mb-0'>
+          <FormLabel className='label'>{label}</FormLabel>
+          <FormControl>
+            <Input
+              className='custom-input'
+              placeholder={placeholder}
+              {...field}
+              type={type}
+            />
+          </FormControl>
+          {fieldState.error ? (
+            <p className='body-text text-red-500 max-w-[250px]'>
+              {fieldState.error.message}
+            </p>
+          ) : null}
+        </FormItem>
+      )}
+    />
+  );
+};
 
 export default FormField;
