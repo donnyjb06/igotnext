790a6921677d372017549aae3d55b4864803e924
DonovanJabbar <djabbar51@gmail.com>
2025-03-24 22:42:55 -0600

    feat: add account completion verification

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,19 @@
+import { verifyUserId } from '@/lib/actions/server-actions';
 import { LayoutProps } from '@/types';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
-const RootLayout: React.FC<LayoutProps> = async ({ children }) => {
-  const { userId } = await auth();
 
-  if (!userId) {
-    redirect('/sign-in');
+const RootLayout: React.FC<LayoutProps> = async ({ children }) => {
+  const { userId: currentUserId } = await auth()
+  if (!currentUserId) {
+    redirect("/sign-in")
+  }
+
+  const isSignUpCompleted = await verifyUserId(currentUserId)
+  if (!isSignUpCompleted) {
+    redirect("/onboarding")
   }
 
   return children;
--- lib/actions/server-actions.ts
+++ lib/actions/server-actions.ts
@@ -1,31 +1,66 @@
-"use server"
+'use server';
 
-import { signUpUserData } from "@/types";
+import { signUpUserData } from '@/types';
 
 export const signUpNewUser = async (userData: signUpUserData) => {
   try {
-    const baseUrl = process.env.NODE_ENV === "production"
-      ? process.env.NEXT_PUBLIC_API_BASE_URL
-      : "http://localhost:3000"
+    const baseUrl =
+      process.env.NODE_ENV === 'production'
+        ? process.env.NEXT_PUBLIC_API_BASE_URL
+        : 'http://localhost:3000';
 
     const response = await fetch(`${baseUrl}/api/auth/save-user`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.API_SECRET_KEY}`
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.API_SECRET_KEY}`,
       },
       body: JSON.stringify(userData),
-    })
+    });
 
     if (!response.ok) {
       const errorData = await response.json();
-      throw new Error(errorData.error || "Failed to sign up user");
+      throw new Error(errorData.error || 'Failed to sign up user');
     }
 
     const data = await response.json();
-    return data
+    return data;
   } catch (error) {
-    console.error("Error has occured when trying to ")
-    throw error
+    console.error('Error has occured when trying to ');
+    throw error;
   }
-}
+};
+
+export const verifyUserId = async (currentUserId: string | null) => {
+  if (!currentUserId) {
+    return false;
+  }
+
+  const baseUrl =
+    process.env.NODE_ENV === 'production'
+      ? process.env.NEXT_PUBLIC_API_URL
+      : 'http://localhost:3000';
+
+  try {
+    const response = await fetch(
+      `${baseUrl}/api/auth/verify-user-id/${currentUserId}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${process.env.API_SECRET_KEY}`,
+        },
+      },
+    );
+
+    const data = await response.json();
+
+    if (!data.success) {
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.error('Error has occured when trying to verify your user ID!');
+    return false;
+  }
+};
